fd06690f45a9d68d7744ad33cf927be56ba3425e
asmitasharma16 <asmitaas73@gmail.com>
2018-08-17 13:44:14 +0530

    learning python

---
+++ Assignment2.py
@@ -0,0 +1,29 @@
+#1.
+print('We are learning Python')
+#2
+s1="HELLO "
+s2="GIT"
+s3=s1+s2 #concatinate s1 and s2
+print(s3)
+print("Acad"+"View")
+#3
+x=int(input("x = "))
+y=int(input("y = "))
+z=int(input("z = "))
+print(x,y,z)
+#4
+print('"Let’s get started"')
+#5
+s="Acadview"
+course="Python"
+fees=5000
+print('s= %s'%(s))
+print('course= %s'%(course))
+print('fees= %d'%(fees))
+#6
+name="Tony Stark"
+salary=1000000
+print('%s' ' %d'%(name,salary))
+
+
+
